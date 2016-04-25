e35327ade7e18e4705eae45f8850b6ac8129d5dc
gcrowder <gregory.j.crowder@gmail.com>
2016-04-25 14:40:51 -0400

    Finish extractor

--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -1,27 +1,27 @@
-# import pytest
-# xfail = pytest.mark.xfail
-# params = pytest.mark.parametrize
-#
-# import textminer.extractor as x
-#
-# @xfail
-# def test_phone_numbers():
-#     text = """Dear Mr. Davis,
-#
-# I got to know of your company through our mutual friend Fiona Williams and the
-# training you offer to graduate students in Advertising.
-#
-# I am a graduate student of Mass Communications with specialization in
-# Advertising.  I am currently pursuing the last year of my course.
-# I would very much like to see firsthand the work environment in an advertising
-# agency.
-#
-# If you would like a reference, my advisor can be reached at (454) 999-1212.
-#
-# You can contact me at (919) 123-4569 at your convenience."""
-#
-#     assert x.phone_numbers(text) == ["(454) 999-1212", "(919) 123-4569"]
-#
+import pytest
+xfail = pytest.mark.xfail
+params = pytest.mark.parametrize
+
+import textminer.extractor as x
+
+@xfail
+def test_phone_numbers():
+    text = """Dear Mr. Davis,
+
+I got to know of your company through our mutual friend Fiona Williams and the
+training you offer to graduate students in Advertising.
+
+I am a graduate student of Mass Communications with specialization in
+Advertising.  I am currently pursuing the last year of my course.
+I would very much like to see firsthand the work environment in an advertising
+agency.
+
+If you would like a reference, my advisor can be reached at (454) 999-1212.
+
+You can contact me at (919) 123-4569 at your convenience."""
+
+    assert x.phone_numbers(text) == ["(454) 999-1212", "(919) 123-4569"]
+
 # ## HARD MODE BEGINS
 #
 # @xfail
---
+++ textminer/extractor.py
@@ -0,0 +1,8 @@
+import re
+
+
+def phone_numbers(string):
+    phone_regex = re.compile(r'(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})')
+    phone_matches_list = phone_regex.findall(string)
+    print(phone_matches_list)
+    return phone_matches_list
